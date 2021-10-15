1a22af609322d40231c88c6d666c33a83ce617a2
dennykurniawan48 <kurniawan.denny.48.com>
2021-10-15 15:23:11 +0700

    remove unused function

--- app/Http/Controllers/Api/Transaction.php
+++ app/Http/Controllers/Api/Transaction.php
@@ -92,40 +92,6 @@ class Transaction extends ApiController
         return response()->json($dataProduct, Response::HTTP_OK);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-
     public function checkout(Request $request)
     {
         $data = [];
